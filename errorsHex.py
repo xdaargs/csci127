f0f376bbb1c24d5e8a9cfe8296a51ecdac5ee39f
xdaargs <44477737+xdaargs@users.noreply.github.com>
2019-04-09 12:16:54 -0400

    Update errorsHex.py

--- errorsHex.py
+++ errorsHex.py
@@ -1,8 +1,9 @@
 #CSci 127 Teaching Staff
 #October 2017
 #A program that converts hex numbers to decimal, but filled with errors...  
-Modified by:  ADD YOUR NAME HERE
+Modified by:  Josh Zhang
 
+     
 define convert(s):
      """ Takes a hex string as input.
      Returns decimal equivalent.
@@ -12,25 +13,25 @@ define convert(s):
      
      for c in s
           total = total * 16
-          ascii = ord(c
-          if ord('0) <= ascii <= ord('9'):
+          ascii = ord(c)
+          if ord('0') <= ascii <= ord('9'):
                #It's a decimal number, and return it as decimal:
-               total = total+ascii - ord('0')
-          elif ord('A") <= ascii <= ord('F'):
+               total += ascii - ord('0')
+          elif ord('A') <= ascii <= ord('F'):
                #It's a hex number between 10 and 15, convert and return:
-               total = total + ascii - ord('A') + 10
-          else ord('a') =< ascii <= ord('f'):
+               total += ascii - ord('A') + 10
+          else ord('a') <= ascii <= ord('f'):
                #Check if they used lower case:
                #It's a hex number between 10 and 15, convert and return:
-               total = total + ascii - ord('a') +++ 10
+               total += ascii - ord('a') + 10
           else:
                #Not a valid number!
                return(-1)
      return(total)
 
 def main()
-    hexString = input("Enter a number in hex: ')
-    prnt("The number in decimal is", convert(hexString))
+    hexString = input("Enter a number in hex: ")
+    print("The number in decimal is", convert(hexString))
 
 
 #Allow script to be run directly:
